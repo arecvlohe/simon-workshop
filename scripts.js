784213353e354f4d09df766ce336daa41c1c580d
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-08-24 20:43:48 -0400

    Final

--- scripts.js
+++ scripts.js
@@ -7,9 +7,37 @@ colors.forEach((button, i) => {
     button.style.filter = "saturate(50%)";
     setTimeout(() => {
       button.style.filter = "saturate(100%)";
+      if (checkUserInput(sequence, state.userClicks) && equalLengths(state)) {
+        ++state.count;
+        state.userClicks = [];
+        playSequence(state);
+      } else {
+        playSequence(state);
+      }
     }, 300);
   });
 });
+
+function checkUserInput(sequence, userClicks) {
+  return userClicks.every(v => {
+    return sequence[v] === v;
+  });
+}
+
+function equalLengths(state) {
+  return state.count === state.userClicks.length;
+}
+
+function playSequence(state) {
+  for (let i = 0; i < state.count; i++) {
+    setTimeout(() => {
+      colors[sequence[i]].style.filter = "saturate(50%)";
+    }, 1000 * (i + 1));
+    setTimeout(() => {
+      colors[sequence[i]].style.filter = "saturate(100%)";
+    }, 1300 * (i + 1));
+  }
+}
 
 const sequence = Array.from({ length: 20 }, (v, k) => {
   return Math.floor(Math.random() * 4);
